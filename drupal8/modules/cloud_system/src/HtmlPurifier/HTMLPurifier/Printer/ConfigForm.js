122316af6a67f83008aa0ffeeff429848f94114a
JuneHu <huyijun1412@gmail.com>
2019-08-31 16:32:26 +0800

    取消sites目录

--- drupal8/modules/cloud_system/src/HtmlPurifier/HTMLPurifier/Printer/ConfigForm.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file
- */
-
-function toggleWriteability(id_of_patient, checked) {
-  document.getElementById(id_of_patient).disabled = checked;
-}
-
-// vim: et sw=4 sts=4.
--- drupal8/modules/cloud_system/src/IpTool/Iterator/v6/Host.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace Drupal\cloud_system\IpTool\Iterator\v6;
-
-class Host extends Address {
-}

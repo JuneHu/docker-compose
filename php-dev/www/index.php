d07cdf672bfed617099c6bb562aa6118e6bdafec
JuneHu <huyijun1412@gmail.com>
2019-09-06 16:25:42 +0800

    php的docker开发环境

---
+++ php-dev/www/index.php
@@ -0,0 +1,18 @@
+<?php
+  // echo 'hello world';
+
+  /*
+  $servername = "mysql";
+  $username = "cloud";
+  $password = "123456";
+   
+  try {
+      $conn = new PDO("mysql:host=$servername;", $username, $password);
+      echo "连接成功"; 
+  }
+  catch(PDOException $e)
+  {
+      echo $e->getMessage();
+  }
+  */
+  phpinfo();
